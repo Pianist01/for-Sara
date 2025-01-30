fa2efaeae7aead5deba5b53eb44ab67352ec5678
Steven Peralta <peralta.steven01@gmail.com>
2025-01-29 23:16:26 -0800

    These files had been created, but I added some changes to classes in html and css file. I also wrote portion of JS code that allows text to appear and dissappear after some time.

--- index.js
+++ index.js
@@ -6,23 +6,50 @@ const container = document.querySelector('.container');
 
 const phraseOne = document.querySelector('p');
 
-let index;
+let index = 0;
+let opacity;
+const opacityEnd = 100;
+const baseOpacity = 0;
 
-const phraseArray = ['Hola mi amor :)', 'Espero que estes bien orita', 'Me imagino que mirando la fecha, sabes que esta pronto de venir', 'Te amo mucho mi amor', 'Este tiempo que hemos tenido juntos han sido unos de mis mas felices', 'Que feliz me haces amor :)', 'Te queria hacer esto porque es mi forma tambien de hacerlo', 'Perdon si es un poco nErRdY para ti amor, jajaja', 'Te amo my love', 'Cuando estes lista, presiona el boton'];
+const phraseArray = ['', 'Hola mi amor :)', 'Espero que estes bien orita', 'Me imagino que mirando la fecha, sabes que esta pronto de venir', 'Te amo mucho mi amor', 'Este tiempo que hemos tenido juntos han sido unos de mis mas felices', 'Que feliz me haces amor :)', 'Te queria hacer esto porque es mi forma tambien de hacerlo', 'Perdon si es un poco nErRdY para ti amor, jajaja', 'Te amo my love', 'Cuando estes lista, presiona el boton'];
 
-index = (index + 1) % phraseArray.length;
 
 window.onload = function() {
-    // phraseOne.style.display = 'block';
-    newSentence();
+    let newSentence = setInterval(() => {
+        index = (index + 1) % phraseArray.length;
+        phraseOne.textContent = phraseArray[index];
+        showUp();
+        console.log(phraseOne);
+        container.append(phraseOne);
+    }, 10000);
+
+    setTimeout(() => {
+    clearInterval(newSentence);
+}, 100000);
 }
 
-const newSentence = () => {
-    let counter = 1;
-    setInterval(() => {
-        const newPhrase = document.createElement('p');
-        newPhrase.textContent = phraseArray[index];
-        container.append(newPhrase);
-        counter++;
-    }, 5000);
+function showUp() {
+    opacity = 0;
+    opacityEnd;
+
+    const animateShow = () => {
+        opacity += 3;
+
+        phraseOne.style.opacity = opacity + '%';
+        if(opacity < opacityEnd) {
+            requestAnimationFrame(animateShow);
+        }
+    }
+    animateShow();
+
+    let goAway = setInterval(() => {
+        const animateAway = () => {
+            opacity -= 3;
+            phraseOne.style.opacity = opacity + '%';
+            if(opacity > baseOpacity) {
+                requestAnimationFrame(animateAway);
+            }
+        }
+        animateAway();
+    }, 8000);
 }
