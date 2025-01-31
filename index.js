7e5e3aebfdb47e1ddba2a5956bdd9e053d768971
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-01-31 15:24:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const phraseOne = document.querySelector('p');
 let index = 0;
 let secondsPassed = 0;
 let opacity = 0;
-let maxOpacity = 100;
-let opacityEnd = 0;
+let increasing = true;
+const duration = 4000;
+const interval = 16;
+const step = interval / duration;
 
 const phraseArray = ['', 'Hola mi amor :)', 'Espero que estes bien orita', 'Me imagino que mirando la fecha, sabes que esta pronto de venir', 'Te amo mucho mi amor', 'Este tiempo que hemos tenido juntos han sido unos de mis mas felices', 'Que feliz me haces amor :)', 'Te queria hacer esto porque es mi forma tambien de hacerlo', 'Perdon si es un poco nErRdY para ti amor, jajaja', 'Te amo my love', 'Cuando estes lista, presiona el boton :)'];
 
@@ -21,47 +23,51 @@ let startUp = function() {
     let newSentence = setInterval(() => {
         index = (index + 1) % phraseArray.length;
         phraseOne.textContent = phraseArray[index];
-        showAndGo();
         console.log(phraseOne);
         container.append(phraseOne);
-    }, 6000);
+    }, 8000);
 
-//     setTimeout(() => {
-//     clearInterval(newSentence);
-// }, 60000);
+    setTimeout(() => {
+    clearInterval(newSentence);
+}, 88000);
 }
 
 startUp();
 
-function showAndGo() {
-    opacity = 0;
-    maxOpacity;
-    setInterval(() => {
-        const showWord = () => {
-            opacity += 3;
-            phraseOne.style.opacity = opacity + '%';
-            if(opacity < maxOpacity) {
-                requestAnimationFrame(showWord);
-            }
+function fadeEffect() {
+    if(increasing) {
+        opacity += step;
+        if(opacity >= 1) {
+            opacity = 1;
+            increasing = false;
         }
-        showWord();
-    }, 3000);
-
-    setInterval(() => {
-        const goAway = () => {
-            opacity -= 3;
-            phraseOne.style.opacity = opacity + '%';
-            if(opacity > opacityEnd) {
-                requestAnimationFrame(goAway);
-            }
+    } else {
+        opacity -= step;
+        if(opacity <= 0) {
+            opacity = 0;
+            increasing = true;
         }
-        goAway();
-    }, 5000);
+    }
+    phraseOne.style.opacity = opacity;
 }
 
-showAndGo();
+setInterval(fadeEffect, interval);
+
 
 setInterval(() => {
     secondsPassed++;
     console.log(`${secondsPassed}`);
 }, 1000);
+
+let remind = setInterval(() => {
+    const reminder = document.createElement('h2');
+    reminder.textContent = 'Sube tu volumne amor :)';
+    reminder.style.color = 'white';
+    reminder.style.opacity = '100';
+    body.append(reminder);
+    console.log('Pon Tu volumne para arriba amor :)');
+}, 5000);
+
+setTimeout(() => {
+    clearInterval(remind);
+}, 9000);
