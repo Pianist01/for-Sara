f3c55f2babf1b25e104a64242366264902c7437a
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-02-04 16:25:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const phraseOne = document.querySelector('p');
 
 const button = document.querySelector('button');
 
-<<<<<<< HEAD
-=======
 const title = document.querySelector('h1');
 
->>>>>>> d50a55a581250c0b936e1ce5634fdacffeae1db7
 let index = 0;
 let secondsPassed = 0;
 let opacity = 0;
@@ -21,17 +18,14 @@ const duration = 4000;
 const interval = 16;
 const step = interval / duration;
 
-let r = 0;
-let g = 0;
-let b = 0;
+let boxShadowSize = 5;
+let growing = true;
 
 const phraseArray = ['', 'Hola mi amor :)', 'Espero que estes bien orita', 'Me imagino que mirando la fecha, sabes que esta pronto de venir', 'Te amo mucho mi amor', 'Este tiempo que hemos tenido juntos han sido unos de mis mas felices', 'Que feliz me haces amor :)', 'Te queria hacer esto porque es mi forma tambien de hacerlo', 'Perdon si es un poco nErRdY para ti amor, jajaja', 'Te amo my love', 'Cuando estes lista, presiona el boton :)'];
 
 button.disabled = true;
 
-body.style.background = `rgb(${r}, ${g}, ${b})`;
-
-button.disabled = true;
+body.style.background = `rgb(0, 0, 0)`;
 
 
 let startUp = function() {
@@ -42,6 +36,8 @@ let startUp = function() {
         container.append(phraseOne);
     }, 8000);
 
+    btnShadow();
+
     setTimeout(() => {
     clearInterval(newSentence);
     button.disabled = false;
@@ -49,16 +45,25 @@ let startUp = function() {
 }, 88000);
 }
 
-button.addEventListener('click', (e) => {
-    console.log('It Works');
-    body.style.backgroundColor = 'pink';
-    button.style.opacity = '0';
-    setInterval(() => {
-        button.style.display = 'none';
-    }, 3000);
-});
-
 startUp();
+
+function btnShadow() {
+    boxShadowSize;
+    setInterval(() => {
+        if(growing) {
+            boxShadowSize += 1;
+            if(boxShadowSize >= 15) {
+                growing = false;
+            }
+        } else {
+            boxShadowSize -= 1;
+            if(boxShadowSize <= 5) {
+                growing = true;
+            }
+        }
+        button.style.boxShadow = `0 0 ${boxShadowSize}px 0`;
+    }, 100);
+}
 
 function fadeEffect() {
     if(increasing) {
@@ -83,28 +88,66 @@ setInterval(fadeEffect, interval);
 setInterval(() => {
     secondsPassed++;
     console.log(`${secondsPassed}`);
-<<<<<<< HEAD
-}, 1000);
-=======
 }, 1000);
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    body.style.opacity = '0';
-    
+    body.style.backgroundColor = '#ffb6c1';
     button.style.opacity = '0';
     title.style.opacity = '0';
+    phraseOne.style.display = 'none';
+    phraseOne.remove();
+    askQuestion();
     setInterval(() => {
         button.style.display = 'none';
     }, 2000);
 });
 
-function changeBackground() {
-    r += 1;
-    g += 1;
-    b += 1;
-    if(r && g && b <= 0) {
-        
+const nextPageArray = ['QUEEEEE PORQUE ESCOJISTE NO???!!!!', 'NO ME QUIERES???', 'AHORA NO HAY REGRESO DE AQUI EN ADELANTE!!!!!', 'BYE!!!'];
+
+function askQuestion() {
+    let opacity = 0;
+    let maxOpacity = 100;
+    const question = document.createElement('p');
+    question.classList.add('question');
+    question.textContent = 'Will You Be My Valentine?';
+
+    
+    
+    const yes = document.createElement('button');
+    yes.classList.add('yes-button');
+    yes.textContent = 'Si';
+
+    const no = document.createElement('button');
+    no.classList.add('no-button');
+    no.textContent = 'No';
+
+    const fadeInEffect = () => {
+        if(opacity < maxOpacity) {
+            opacity += 3;
+            question.style.opacity = opacity + '%';
+            yes.style.opacity = opacity + '%';
+            no.style.opacity = opacity + '%';
+            requestAnimationFrame(fadeInEffect);
+        }
     }
+
+    fadeInEffect();
+
+    container.append(question, yes, no);
+
+    no.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('This button works');
+        body.style.background = 'red';
+        question.textContent = '';
+        setInterval(() => {
+            index = (index + 1) % nextPageArray.length;
+            question.textContent = nextPageArray[index];
+        }, 3000)
+
+        setTimeout(() => {
+            window.close();
+        }, 12000);
+    });
 }
->>>>>>> d50a55a581250c0b936e1ce5634fdacffeae1db7
