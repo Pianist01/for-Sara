0c7258bacb66f65adbac6e307b98c26004608b2d
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-02-10 16:24:59 -0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,5 +1,15 @@
 console.log('This works');
 
+const activityOne = document.querySelector('.activity-one');
+const activityTwo = document.querySelector('.activity-two');
+const activityThree = document.querySelector('.activity-three');
+
+window.onload = function() {
+    activityOne.style.opacity = '100';
+    activityTwo.style.opacity = '100';
+    activityThree.style.opacity = '100';
+}
+
 const buttonOne = document.querySelector('.btn-one');
 buttonOne.addEventListener('click', (e) => {
     e.preventDefault();
