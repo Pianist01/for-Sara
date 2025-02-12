afab03bb0badbf8c4459d8a2829cd921bce071b6
Steven Peralta <peralta.steven01@gmail.com>
2025-02-11 22:17:04 -0800

    Wrote more code to allow text to appear and move up when user scrolls down a certain amount of pixels, also styled more of the page, and moved the images to the image folder.

--- paint.js
+++ paint.js
@@ -2,17 +2,17 @@ console.log('Script is working');
 
 const title = document.querySelector('h1');
 const mainContainer = document.querySelector('main');
-const contentContainer = document.createElement('div');
-contentContainer.classList.add('info-one');
+const contentContainerOne = document.createElement('div');
+contentContainerOne.classList.add('info-one');
 
 window.onload = function() {
     title.style.opacity = '100';
-    contentContainer.style.gridRow = 2/4;
-    contentContainer.style.height = '100%';
-    mainContainer.append(contentContainer);
+    contentContainerOne.style.gridRow = '2/4';
+    contentContainerOne.style.height = '100%';
+    mainContainer.append(contentContainerOne);
 }
 
-const txtArray = ['', 'Para este plan, tenemos desayuno, una actividad, y cena', 'No hay horario en cuando tenemos que hacer todo, menos la actividad'];
+const txtArray = ['', 'Para este plan, tenemos desayuno, una actividad, y cena', 'No hay horario en cuando tenemos que hacer todo, menos la actividad', 'Para empezar, te voy a ensenar en donde vamos a desayunar para esta cita', 'Desayuno'];
 
 let index = 0;
 // index = (index + 1) % txtArray.length;
@@ -28,8 +28,8 @@ window.addEventListener('scroll', function() {
         infoText.classList.add(`info-text${textNumber += 1}`);
         infoText.textContent = txtArray[index += 1];
         infoText.style.opacity = '100';
-        contentContainer.style.opacity = '100';
-        contentContainer.append(infoText);
+        contentContainerOne.style.opacity = '100';
+        contentContainerOne.append(infoText);
         console.log(infoText);
     }
 });
