029993439c0a1f493134ad04ef8d889b885c27ba
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-02-18 16:06:29 -0800

    Update paint.js

--- paint.js
+++ paint.js
@@ -7,12 +7,12 @@ contentContainerOne.classList.add('info-one');
 
 window.onload = function() {
     title.style.opacity = '100';
-    contentContainerOne.style.gridRow = '2/13';
+    contentContainerOne.style.gridRow = '2/16';
     contentContainerOne.style.height = '100%';
     mainContainer.append(contentContainerOne);
 }
 
-const dateOneArray = ['', 'Para este plan, tenemos desayuno, una actividad, y cena', 'No hay horario en cuando tenemos que hacer todo, menos la actividad', 'Para empezar, te voy a ensenar en donde vamos a desayunar para esta cita', 'Desayuno', 'Chuponcito', 'El Chuponzito es desayuno Mexicano con cosas como: Chilquiles, Sopes de Desayuno, Omelets, Burritos de Desayuno, Sandwich de Desayuno y mas. Aqui esta el link para que mires:', 'Actividad'];
+const dateOneArray = ['', 'Para este plan, tenemos desayuno, una actividad, y cena', 'No hay horario en cuando tenemos que hacer todo, menos la actividad', 'Para empezar, te voy a ensenar en donde vamos a desayunar para esta cita', 'Desayuno', 'Chuponcito', 'El Chuponzito es desayuno Mexicano con cosas como: Chilquiles, Sopes de Desayuno, Omelets, Burritos de Desayuno, Sandwich de Desayuno y mas. Aqui esta el link para que mires:', 'Actividad', 'Seven Falls Massage & Spa', 'Seven Falls Massage & Spa es un lugar donde puedemos agarrar un masaje como pareja. Si esojes este hariamos la de parejas de una hora que incluye masaje del cuerpo. Aqui esta la pagina para que tengas mas informacion del lugar:', 'Cena', 'Buona Forchetta', 'Aqui amor es el lugar donde ibamos ir el Sabado pasado donde teniamos que tener reservacion. Es comida Italiana entonces tienen pasta, lasgana, pizza, etc. Igual, aqui esta el link para que mires el menu completo y tambien las fotos:'];
 
 const dateOneLink = document.createElement('p');
 dateOneLink.classList.add('food-link');
@@ -86,4 +86,99 @@ const activityImageBox = document.createElement('div');
 activityImageBox.classList.add('activity-imgBox');
 const activityImage = document.createElement('img');
 activityImage.src = 'img/massageImage.jpg';
+activityImage.classList.add('activity-image')
 activityImageBox.append(activityImage);
+contentContainerOne.append(activityImageBox);
+
+const activityOneLink = document.createElement('p');
+activityOneLink.classList.add('activity-link');
+activityOneLink.textContent = 'La pagina del masaje para que mires amor :)';
+contentContainerOne.append(activityOneLink);
+
+const activityObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    activityImageBox.classList.add('visible');
+    observer.disconnect();
+  }
+}, {
+  threshold: 0.1
+});
+
+activityObserver.observe(activityImageBox);
+
+const actLinkObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    activityOneLink.classList.add('visible');
+    observer.disconnect();
+  }
+  }, {
+    threshold: 0.1
+});
+
+actLinkObserver.observe(activityOneLink);
+
+activityOneLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.open('https://www.sevenfallsspa.com/services-lakeforest', '_blank');
+});
+
+const dinnerBox = document.createElement('div');
+dinnerBox.classList.add('dinner-box');
+const dinnerImage = document.createElement('img');
+dinnerImage.classList.add('dinner-image');
+dinnerImage.src = 'img/dateonedinner.jpg';
+dinnerBox.append(dinnerImage);
+contentContainerOne.append(dinnerBox);
+
+const dinnerLink = document.createElement('p');
+dinnerLink.classList.add('dinner-link');
+dinnerLink.textContent = 'Menu para mi princesa :)';
+contentContainerOne.append(dinnerLink);
+
+const returnButton = document.createElement('button');
+returnButton.classList.add('return-btn');
+returnButton.textContent = 'Regresar';
+contentContainerOne.append(returnButton);
+
+const dinnerObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    dinnerBox.classList.add('visible');
+    observer.disconnect();
+  }
+  }, {
+    threshold: 0.1
+});
+
+dinnerObserver.observe(dinnerBox);
+
+const dinnerLinkObserver =  new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    dinnerLink.classList.add('visible');
+    observer.disconnect();
+  }
+  }, {
+    threshold: 0.1
+});
+
+dinnerLinkObserver.observe(dinnerLink);
+
+const btnObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    returnButton.classList.add('visible');
+    observer.disconnect()
+  }
+  }, {
+    threshold: 0.1
+});
+
+btnObserver.observe(returnButton);
+
+dinnerLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.open('https://www.yelp.com/biz/buona-forchetta-newport-beach-2', '_blank');
+});
+
+returnButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = 'activities.html';
+});
