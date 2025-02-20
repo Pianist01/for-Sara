f60689cffad6546892e585826bb40b95ddcb4c6e
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-02-19 16:23:07 -0800

    Update light.js

--- light.js
+++ light.js
@@ -12,7 +12,7 @@ window.onload = function() {
     mainContainer.append(contentContainerOne);
 }
 
-const dateTwoArray = ['', 'Hola amor :), para esta cita igual tenemos desayuno, nuestra actividad, y cena', 'Igual como el otro, no tenemos horario en cuando tenemos que hacer todo, menos para la actividad', 'Ahora para el desayuno mi amor', 'Desayuno', 'Burnt Crumbs', 'Burnt Crumbs es un restuarante Americano que tiene comida como omeletes, pancakes gordas, galletas, burritos, y sandwich, y mas. Aqui esta el link:'];
+const dateTwoArray = ['', 'Hola amor :), para esta cita igual tenemos desayuno, nuestra actividad, y cena', 'Igual como el otro, no tenemos horario en cuando tenemos que hacer todo, menos para la actividad', 'Ahora para el desayuno mi amor', 'Desayuno', 'Burnt Crumbs', 'Burnt Crumbs es un restuarante Americano que tiene comida como omeletes, pancakes gordas, galletas, burritos, y sandwich, y mas. Aqui esta el link:', 'Actividad', 'Astra Lumina', 'Astra Lumina es un lugar donde literalmente tienen luzes en un jardin y se mira como que estas en el espacio explorando las estrellas. Toma una hora para caminar todo el parque y es lugar muy bueno para tomar fotos amor. Aqui esta el link para que mires todas las fotos en su pagina:', 'Cena', 'INI Ristorante by Kei Concepts', 'INI Ristorante es un lugar italiano con conceptos Japoneses tambien. Tiene la comida italiana tipica, pero tambien versiones con un toque Japones. Entonces en alguna pasta le ponen un huevo estrellado encima como lo hacen en la sopas en Japon. Como siempre amor, aqui esta el link:'];
 
 const dateOneLink = document.createElement('p');
 dateOneLink.classList.add('food-link');
@@ -79,4 +79,105 @@ foodObserver.observe(dateOneLink);
 dateOneLink.addEventListener('click', (e) => {
     e.preventDefault();
     window.open('https://www.yelp.com/biz/burnt-crumbs-irvine', '_blank');
+});
+
+const activityImageBox = document.createElement('div');
+activityImageBox.classList.add('activity-imgBox');
+const activityImage = document.createElement('img');
+activityImage.src = 'img/datetwoactivity.webp';
+activityImage.classList.add('activity-image')
+activityImageBox.append(activityImage);
+contentContainerOne.append(activityImageBox);
+
+const activityOneLink = document.createElement('p');
+activityOneLink.classList.add('activity-link');
+activityOneLink.textContent = 'La pagina del masaje para que mires amor :)';
+contentContainerOne.append(activityOneLink);
+
+const activityObserver = new IntersectionObserver((entries, observer) => {
+    if(entries[0].isIntersecting) {
+      activityImageBox.classList.add('visible');
+      observer.disconnect();
+    }
+  }, {
+    threshold: 0.1
+  });
+
+activityObserver.observe(activityImageBox);
+
+const actLinkObserver = new IntersectionObserver((entries, observer) => {
+    if(entries[0].isIntersecting) {
+      activityOneLink.classList.add('visible');
+      observer.disconnect();
+    }
+    }, {
+      threshold: 0.1
+  });
+  
+actLinkObserver.observe(activityOneLink);
+
+activityOneLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.open('https://astralumina.com/los-angeles/?utm_source=google&utm_medium=localcards&utm_campaign=138685_LAX', '_blank');
+});
+
+const dinnerBox = document.createElement('div');
+dinnerBox.classList.add('dinner-box');
+const dinnerImage = document.createElement('img');
+dinnerImage.classList.add('dinner-image');
+dinnerImage.src = 'img/inirestaurant.jpg';
+dinnerBox.append(dinnerImage);
+contentContainerOne.append(dinnerBox);
+
+const dinnerLink = document.createElement('p');
+dinnerLink.classList.add('dinner-link');
+dinnerLink.textContent = 'Menu para mi princesa :)';
+contentContainerOne.append(dinnerLink);
+
+const returnButton = document.createElement('button');
+returnButton.classList.add('return-btn');
+returnButton.textContent = 'Regresar';
+contentContainerOne.append(returnButton);
+
+const dinnerObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    dinnerBox.classList.add('visible');
+    observer.disconnect();
+  }
+  }, {
+    threshold: 0.1
+});
+
+dinnerObserver.observe(dinnerBox);
+
+const dinnerLinkObserver =  new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    dinnerLink.classList.add('visible');
+    observer.disconnect();
+  }
+  }, {
+    threshold: 0.1
+});
+
+dinnerLinkObserver.observe(dinnerLink);
+
+const btnObserver = new IntersectionObserver((entries, observer) => {
+  if(entries[0].isIntersecting) {
+    returnButton.classList.add('visible');
+    observer.disconnect()
+  }
+  }, {
+    threshold: 0.1
+});
+
+btnObserver.observe(returnButton);
+
+dinnerLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.open('https://www.yelp.com/biz/ini-ristorante-by-kei-concepts-fountain-valley-3?osq=ini+resturante', '_blank');
+});
+
+returnButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = 'activities.html';
 });
